f8d07bf0a3dc625acee7a9a6bda0e73b10ee9cee
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2021-06-01 06:51:13 +0200

    Update musicEngine.py

--- musicEngine.py
+++ musicEngine.py
@@ -2,6 +2,8 @@ import os
 import random
 from pygame import mixer
 
+
+# Not really formated for this game but it works pulled from my rpg game where there should be diffrent music for fights and just walking currently on hold
 class musicEngine:
     def __init__(self):
         self.vol = 0.1
@@ -9,10 +11,16 @@ class musicEngine:
         self.currently_playing = {"name": "", "state": "n/c"}
         self.normal_dir = (os.path.dirname(os.path.realpath(__file__)) + "\\" + "music\\normal")
         self.normal_listdir = os.listdir(self.normal_dir)
+        if not self.normal_listdir:
+            self.disabled = True
+        else:
+            self.disabled = False
 
     def start(self):
         is_busy = mixer.music.get_busy()
         mixer.music.set_volume(self.vol)
+        if self.disabled:
+            return "NoDirSong"
         if self.MUSIC_STATE != self.currently_playing["state"]:
             mixer.stop()
             if self.MUSIC_STATE == "normal":
