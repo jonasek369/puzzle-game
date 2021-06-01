36de55f5cbcd7d9ce4ad4bb01f54c9e141ddad84
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2021-06-01 06:50:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import threading
 import time
 import keyboard
+from colorama import Fore
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
@@ -12,14 +13,23 @@ from mapGenerator import mapGenerator
 
 # keyboard.press('f11')
 os.system("mode 800")
+
+# some people meight be confused why am i clearing empty window on start with cls its beacuse
+# if you dont do this the text cant be color just wold look like 53dtest insted of colored text
 os.system("cls")
+
+# init the mixer for it to work not sure if its needed i do it
 mixer.init()
-cr = "+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+"
+
+# just decoration line
+cr = "+" + "-" * 234 + "+"
+
+# set volumes
 max_vol = 1
 min_vol = 0.01
 curr_vol = 0.01
 
-
+# class for player
 class player:
     def __init__(self):
         self.position = ""
@@ -29,7 +39,7 @@ alive = True
 p = player()
 mE = musicEngine()
 
-mapGen = mapGenerator(current_level=1, preset="abcdefghijk")
+mapGen = mapGenerator(current_level=1, preset="abcdefghijk")  # change preset to make map bigger/smaller
 mapGen.generate_map(debugger=False)
 mapGen.generate_bioms(debugger=False)
 
@@ -38,6 +48,10 @@ with open(f"map.json", "w") as save:
 
 
 def starting_pos(debugger=False):
+    """
+    :param debugger: Bool : T/F if you want to see debugger info for normal game False
+    :return:
+    """
     if debugger:
         start = time.time()
     while True:
@@ -54,6 +68,7 @@ mE.vol = 0.01
 
 
 def start_threads():
+    # starts music engine as thread so the game can run
     song_thread = threading.Thread(target=music_player)
     song_thread.start()
     time.sleep(0.2)
@@ -61,6 +76,7 @@ def start_threads():
 
 
 def die():
+    # kills all threads
     global alive
     alive = False
 
@@ -70,7 +86,10 @@ def music_player():
     while True:
         if not alive:
             break
-        mE.start()
+        engine = mE.start()
+        if engine == "NoDirSong":
+            print("no songs in current_folder/music/normal turning off music engine")
+            break
 
 
 def setting_menu():
@@ -178,6 +197,13 @@ def check_for_boxes():
 
 
 def collision_check(direction):
+    """
+    :param direction: Str : example up
+    :return:
+    """
+
+    # really scuffed needs hard rework worked on this in night and its bad
+    # but it dose its job so i guess its ok for now
     if mapGen.map[p.position][direction] is not None:
         if mapGen.map[mapGen.map[p.position][direction]]["biom"] != "locked_box":
             if mapGen.map[mapGen.map[p.position][direction]] is not None:
@@ -206,8 +232,31 @@ def collision_check(direction):
             return False
 
 
-def currently_playing(song):
+def currently_playing(song, rainbow=False):
+    """
+    :param rainbow: Bool : rainbow effect not the best but for fun why not
+    :param song: Str : Song name
+    :return:
+    """
+    # formats song name (just removes extension for now)
     formated = ""
+    colors = [
+        Fore.RED,
+        Fore.GREEN,
+        Fore.YELLOW,
+        Fore.BLUE,
+        Fore.MAGENTA,
+        Fore.CYAN,
+        Fore.WHITE,
+        Fore.LIGHTBLACK_EX,
+        Fore.LIGHTRED_EX,
+        Fore.LIGHTGREEN_EX,
+        Fore.LIGHTYELLOW_EX,
+        Fore.LIGHTBLUE_EX,
+        Fore.LIGHTMAGENTA_EX,
+        Fore.LIGHTCYAN_EX,
+        Fore.LIGHTWHITE_EX
+    ]
     extension = [
         ".mp3",
         ".wav",
@@ -219,10 +268,17 @@ def currently_playing(song):
 
     if len(song) == 0:
         formated = song
-    return formated
+    if rainbow:
+        ranb = ""
+        for i in formated:
+            ranb += random.choice(colors) + i + Fore.WHITE
+        return ranb
+    else:
+        return formated
 
 
 def moving():
+    # main gameplay fuction
     global alive, newpos_save
     while True:
         win = check_for_boxes()
@@ -240,7 +296,7 @@ def moving():
             pass
         os.system("cls")
         print("your position", p.position, "level", mapGen.max_boxes)
-        print("playing:", currently_playing(mE.currently_playing["name"]))
+        print("playing:", currently_playing(mE.currently_playing["name"], rainbow=False))
         print(cr)
         print(mapGen.show_map(p.position))
         print(cr)
@@ -251,11 +307,12 @@ def moving():
         print("\u001b[37m[\u001b[32mR\u001b[37m] Reset")
         print("\u001b[37m[\u001b[32mB\u001b[37m] Back to menu")
         print(cr)
-        time.sleep(0.15)
+        time.sleep(0.15)  # refresh time
         wait_until_play()
 
 
 def wait_until_play():
+    # waits until player press some button i use this so the command line doesn't flicker
     while True:
         if keyboard.is_pressed("b"):
             os.system("cls")
@@ -292,6 +349,7 @@ def wait_until_play():
                 return
 
 
+# creates starting pos saves it and start all threads
 p.position = starting_pos()
 oldppos = p.position
 start_threads()
